65fd1df9761e082ba2ab8d444c3b9e72623c566b
Machy <johnmacky.llego@gmail.com>
2017-05-10 14:37:39 +0800

    Signed-off-by: Machy <johnmacky.llego@gmail.com>

--- crud/urls.py
+++ crud/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-#from app import views
 
 urlpatterns = [
-#    url(r'^$', views.index, name = 'index'),
-#    url(r'^app/', include('app.urls')),
+    url(r'^', include('servers.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^servers/', include('servers.urls')),
 ]
